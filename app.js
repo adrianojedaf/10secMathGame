7c9c7e234a057e354179c518b8dfaac5409d17b6
adrianojedaf <adrianojedaf@hotmail.com>
2022-08-21 16:49:48 +0200

    update app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
     //Global variables
     
     var currentQuestion;
+    var secondsLeft = 10;
     var score = 0;
     var highestScore = 1;
+    var interval;
     
     //Functions
 
@@ -28,6 +30,7 @@ $(document).ready(function() {
             updateQuestion();
             emptyInput();
             updateScore();
+            getSecondsLeft(1);
         } else {
 
         }
@@ -40,16 +43,56 @@ $(document).ready(function() {
 
     function emptyInput() {
         $('input').val('');
-    }
+    };
 
     function updateScore() {
         score += 1;
         $('.score span').text(score);
+    };
+
+    function getSecondsLeft(amount) {
+        if(secondsLeft < 10) { // <10 so in the beginning it will not add 1Sec. to the 10 so it will not become 11.
+            secondsLeft += amount;
+            $('.time span').text(secondsLeft);
+        }
+    };
+
+    function updateHighestScore() {
+        if(score > highestScore) {
+            highestScore = score;
+            $('.highest-score span').text(score);
+        } else {
+            $('.highest-score span').text(highestScore);
+        }
     }
+
+    function startGame() {
+        if(!interval) {
+
+            if(secondsLeft == 0) {
+                getSecondsLeft(10);
+                score = 0;
+            };
+
+            interval = setInterval(function getSecondsLeft() {
+                console.log("interval: " + interval);
+                secondsLeft--;
+                $('.time span').text(secondsLeft);
+        
+                if (secondsLeft === 0) {
+                    clearInterval(interval);
+                    updateHighestScore();
+                    interval = undefined;
+                };
+                
+            }, 1000);
+        }
+    };
 
     //Events
 
     $('form input').on('keyup', function() {
+        startGame();
         checkAnswer(Number($(this).val()), currentQuestion.answer);
     });
 
@@ -57,6 +100,7 @@ $(document).ready(function() {
     //Game Sequence
 
     updateQuestion();
+    checkGameStart();
 
     
 
